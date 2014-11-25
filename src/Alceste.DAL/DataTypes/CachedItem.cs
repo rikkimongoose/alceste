a6b67acba4f994a569200dc3a2df6326b0efea68
RikkiMongoose <alexander.a.teut@gmail.com>
2014-11-25 09:10:36 +0300

    + add packages

--- src/Alceste.DAL/DataTypes/CachedItem.cs
+++ src/Alceste.DAL/DataTypes/CachedItem.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Alceste.DAL.DataTypes
 {
-    class CachedItem
+    public class CachedItem
     {
+        public CachedItem()
+        {
+            ChannelsCount = 1;
+            ChannelNumber = 1;
+        }
+        
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid AccountId { set; get; }
+
+        public string FileItemId { get; set; }
+        
+        public string FilePath { get; set; }
+
+        public double? Length { get; set; }
+
+        public int ChannelsCount { get; set; }
+
+        public int ChannelNumber { get; set; }
+
+        public string WaveFormat { get; set; }
+
+        public virtual ICollection<CachedItemImage> Images { get; set; }
     }
 }
